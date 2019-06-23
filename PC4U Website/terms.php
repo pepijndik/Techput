00f53a3f255d625816fee0908c1599f760d33701
pepijn <pepijn@pdik.nl>
2019-06-23 14:22:08 +0100

    Commit

---
+++ PC4U Website/terms.php
@@ -0,0 +1,31 @@
+<?php   
+session_start(); 
+require_once('forms/connection.php');
+?>
+
+<html>
+    <head>
+            <title>Terms & policies</title>
+            <link rel="stylesheet" href="css/stylesheetTerms.css">
+    </head>
+        <body>
+            <div id="navigation">
+                <ul>                        
+                        <li><a href="index.php">Login</a></li>
+                        <li><a href="register.php">Register</a></li>                           
+                </ul>
+            </div>
+                <div class="termsbox">
+                    <p><h1>Terms & Policies</h1></p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur quam ligula, feugiat eu pulvinar quis, mattis eu augue. Quisque id sem ligula. Sed tincidunt luctus imperdiet. Pellentesque ut posuere felis. Proin in eros laoreet, vehicula justo vel, sodales orci. Vestibulum lacinia aliquet leo eget fermentum. Curabitur lacinia enim lectus, vitae ullamcorper erat porta sed. Maecenas ut rutrum erat, sit amet viverra nulla. Quisque laoreet blandit turpis, vitae mattis risus laoreet in. Sed consequat ligula non dui dapibus hendrerit. Mauris pharetra egestas dolor dictum porta. Praesent laoreet placerat massa vitae cursus. Donec turpis lacus, mollis vel eros at, dignissim congue elit. Nunc commodo nibh augue, eu cursus neque sagittis id. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+
+                    <p>Proin id lectus mauris. Cras non sapien eu turpis tempor condimentum ac eget orci. Vestibulum eget elit sem. Nullam eu pellentesque leo. Donec in felis lacinia, fermentum purus a, molestie nisi. Donec posuere aliquam nisl vitae lobortis. Aenean viverra leo nec sollicitudin faucibus. Aliquam gravida mauris odio, a malesuada velit sodales ut. Cras mattis mollis mauris eu malesuada. Donec consectetur nec orci consequat feugiat. Ut cursus tincidunt arcu sed laoreet. Proin a tortor augue. Morbi id ligula in mi consequat consectetur.</p>
+
+                    <p>Nullam sit amet ligula varius, convallis lectus quis, porttitor diam. Phasellus gravida eget enim et placerat. Duis nec turpis cursus, pharetra quam ac, posuere lacus. Proin aliquam est sed urna ultrices, vitae consectetur ipsum dictum. Phasellus vitae molestie est. Maecenas commodo id orci non bibendum. Nulla facilisi. Cras neque nisi, maximus at enim mattis, tempus aliquet eros. Vestibulum eget pretium ex. Nunc at ligula diam.</p>
+
+                    <p>Phasellus congue nulla vel felis pulvinar, et molestie arcu luctus. Integer rutrum nisi tortor, congue pulvinar ligula bibendum eget. Donec porta, orci vitae pretium blandit, tellus sapien finibus felis, vel semper purus mi vitae velit. Morbi dapibus tellus nec odio lobortis, a pellentesque lacus aliquet. Fusce placerat in elit eu aliquam. Vivamus sodales ultricies dui, at facilisis orci fringilla non. Aenean gravida arcu nibh, in efficitur turpis ultricies ut. Vivamus vehicula et erat vitae rhoncus. Proin auctor interdum tempor. Suspendisse sagittis sit amet nisi eget interdum. Mauris tincidunt odio et ante ornare fringilla. Nulla sit amet nunc blandit, molestie nulla in, ornare lectus. Integer quam augue, iaculis consequat efficitur id, lobortis dictum lectus. Suspendisse sodales euismod laoreet.</p>
+
+                    <p>Nullam maximus neque et quam egestas, ac viverra nisi laoreet. Etiam euismod tortor at mauris egestas ornare. Praesent lacinia arcu eget laoreet blandit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In hac habitasse platea dictumst. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer venenatis erat nisl, et rhoncus neque tincidunt sit amet. Vivamus ante ex, ultricies aliquam sem quis, vulputate viverra dolor. Aenean eget mauris odio. In facilisis pellentesque sagittis.</p>
+                </div>  
+    </body>
+</html>
